83206fe3ea192f21dbb7ebe7acfe560669fa2d2d
William Green <20917127+wbgreen0405@users.noreply.github.com>
2025-06-23 16:51:23 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,14 +1,23 @@
-import os
+import yaml
 
-def get_settings(batch_size=6, epochs=2, encoder='resnet34'):
-    return {
-        'BATCH_SIZE': batch_size,
-        'EPOCHS': epochs,
-        'ENCODER': encoder
-    }
+P = {
+    'EPOCHS': 30,
+    'BACKBONE': 'EfficientNetB0',
+    'NFOLDS': 5,
+    'SEED': 0,
+    'VERBOSE': 0,
+    'DISPLAY_PLOT': True,
+    'BATCH_COE': 8,
+    'TILING': [1024,512],
+    'DIM': 512,
+    'DIM_FROM': 1024,
+    'LR': 5e-4,
+    'OVERLAPP': True,
+    'STEPS_COE': 1,
+}
 
-DATA_ORIG_PATH = '../input/hubmap-kidney-segmentation'
-DATA_PATH = '../input/hubmap-256x256'
-PATH_TRAIN = os.path.join(DATA_PATH, 'train')
-PATH_MASKS = os.path.join(DATA_PATH, 'masks')
-PATH_SUBMISSION = os.path.join(DATA_ORIG_PATH, 'sample_submission.csv')
+P['DIM'] = P['TILING'][1]
+P['DIM_FROM'] = P['TILING'][0]
+
+with open('params.yaml', 'w') as file:
+    yaml.dump(P, file)
