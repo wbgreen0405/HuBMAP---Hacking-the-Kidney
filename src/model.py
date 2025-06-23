19cae3e679932d1be7792c66df82568c3dc637d3
William Green <20917127+wbgreen0405@users.noreply.github.com>
2025-06-23 16:52:29 -0500

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,72 +1,12 @@
-import tensorflow as tf
-from tensorflow.keras.layers import (
-    Input, Conv2D, BatchNormalization, LeakyReLU, Dropout,
-    MaxPooling2D, Conv2DTranspose, concatenate
-)
-from tensorflow.keras.models import Model
+from keras_unet_collection import models
 
-def conv2d(filters, kernel_size=3, activation=True, dropout_rate=0.2):
-    def layer(x):
-        x = Conv2D(filters, (kernel_size, kernel_size), padding='same')(x)
-        x = BatchNormalization()(x)
-        if activation:
-            x = LeakyReLU(0.01)(x)
-        if dropout_rate:
-            x = Dropout(dropout_rate)(x)
-        return x
-    return layer
-
-def conv2dtranspose(filters, kernel_size=2):
-    def layer(x):
-        return Conv2DTranspose(filters, (kernel_size, kernel_size), strides=(2, 2), padding='same')(x)
-    return layer
-
-def UNetPP(input_shape=(256, 256, 3), num_classes=1, number_of_filters=2, dropout_rate=0.2):
-    """UNet++ implementation (nested skip connections, 4 levels deep, patch size 256x256)."""
-    inputs = Input(input_shape)
-
-    # Encoder
-    x00 = conv2d(16 * number_of_filters, dropout_rate=dropout_rate)(inputs)
-    x00 = conv2d(16 * number_of_filters, dropout_rate=dropout_rate)(x00)
-    p0 = MaxPooling2D(pool_size=(2, 2))(x00)
-
-    x10 = conv2d(32 * number_of_filters, dropout_rate=dropout_rate)(p0)
-    x10 = conv2d(32 * number_of_filters, dropout_rate=dropout_rate)(x10)
-    p1 = MaxPooling2D(pool_size=(2, 2))(x10)
-
-    x20 = conv2d(64 * number_of_filters, dropout_rate=dropout_rate)(p1)
-    x20 = conv2d(64 * number_of_filters, dropout_rate=dropout_rate)(x20)
-    p2 = MaxPooling2D(pool_size=(2, 2))(x20)
-
-    x30 = conv2d(128 * number_of_filters, dropout_rate=dropout_rate)(p2)
-    x30 = conv2d(128 * number_of_filters, dropout_rate=dropout_rate)(x30)
-    p3 = MaxPooling2D(pool_size=(2, 2))(x30)
-
-    m = conv2d(256 * number_of_filters, dropout_rate=dropout_rate)(p3)
-    m = conv2d(256 * number_of_filters, dropout_rate=dropout_rate)(m)
-
-    # Decoder (nested skip connections)
-    x31 = conv2dtranspose(128 * number_of_filters)(m)
-    x31 = concatenate([x31, x30])
-    x31 = conv2d(128 * number_of_filters, dropout_rate=dropout_rate)(x31)
-    x31 = conv2d(128 * number_of_filters, dropout_rate=dropout_rate)(x31)
-
-    x22 = conv2dtranspose(64 * number_of_filters)(x31)
-    x22 = concatenate([x22, x20])
-    x22 = conv2d(64 * number_of_filters, dropout_rate=dropout_rate)(x22)
-    x22 = conv2d(64 * number_of_filters, dropout_rate=dropout_rate)(x22)
-
-    x13 = conv2dtranspose(32 * number_of_filters)(x22)
-    x13 = concatenate([x13, x10])
-    x13 = conv2d(32 * number_of_filters, dropout_rate=dropout_rate)(x13)
-    x13 = conv2d(32 * number_of_filters, dropout_rate=dropout_rate)(x13)
-
-    x04 = conv2dtranspose(16 * number_of_filters)(x13)
-    x04 = concatenate([x04, x00])
-    x04 = conv2d(16 * number_of_filters, dropout_rate=dropout_rate)(x04)
-    x04 = conv2d(16 * number_of_filters, dropout_rate=dropout_rate)(x04)
-
-    output = Conv2D(num_classes, kernel_size=(1, 1), activation='sigmoid')(x04)
-
-    model = Model(inputs=inputs, outputs=output)
+def build_unet3plus(dim, backbone, weights='imagenet'):
+    model = models.unet_3plus_2d(
+        (dim, dim, 3), n_labels=1, 
+        filter_num_down=[64, 128, 256, 512, 1024],
+        filter_num_skip='auto', filter_num_aggregate='auto',
+        activation='GELU', output_activation='Sigmoid', batch_norm=True,
+        backbone=backbone, weights=weights, 
+        deep_supervision=True, pool=True, unpool=True, name='unet3plus'
+    )
     return model
