a1ff8bcc7f147599a0c79a2092716346db9a5fe5
William Green <20917127+wbgreen0405@users.noreply.github.com>
2025-06-23 16:51:42 -0500

    Update losses.py

--- src/losses.py
+++ src/losses.py
@@ -1,11 +1,27 @@
 import tensorflow as tf
-import tensorflow.keras.backend as K
+from tensorflow.keras import backend as K
 
-def dice_coeff(y_true, y_pred, smooth=1):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+def dice_coe(output, target, axis=None, smooth=1e-10):
+    output = tf.dtypes.cast(tf.math.greater(output, 0.5), tf.float32)
+    target = tf.dtypes.cast(tf.math.greater(target, 0.5), tf.float32)
+    inse = tf.reduce_sum(output * target, axis=axis)
+    l = tf.reduce_sum(output, axis=axis)
+    r = tf.reduce_sum(target, axis=axis)
+    dice = (2. * inse + smooth) / (l + r + smooth)
+    dice = tf.reduce_mean(dice, name='dice_coe')
+    return dice
 
-def dice_loss(y_true, y_pred):
-    return 1 - dice_coeff(y_true, y_pred)
+def tversky(y_true, y_pred, alpha=0.7, beta=0.3, smooth=1):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1 - y_pred_pos))
+    false_pos = K.sum((1 - y_true_pos) * y_pred_pos)
+    return (true_pos + smooth) / (true_pos + alpha * false_neg + beta * false_pos + smooth)
+
+def tversky_loss(y_true, y_pred):
+    return 1 - tversky(y_true, y_pred)
+
+def focal_tversky_loss(y_true, y_pred, gamma=0.75):
+    tv = tversky(y_true, y_pred)
+    return K.pow((1 - tv), gamma)
