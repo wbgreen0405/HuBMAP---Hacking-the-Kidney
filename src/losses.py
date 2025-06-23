8470ee68c6d38732db0416db79d6d274bd125109
William Green <20917127+wbgreen0405@users.noreply.github.com>
2025-06-23 16:00:59 -0500

    Create losses.py

---
+++ src/losses.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+def dice_coeff(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+    return 1 - dice_coeff(y_true, y_pred)
